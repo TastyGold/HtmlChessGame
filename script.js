d3cfed2fb876061dba5a798f4191d1e0632384f1
Phoebe J <tastygold99513@gmail.com>
2024-10-20 23:27:14 +1100

    pieces on the board :)

--- script.js
+++ script.js
@@ -2,9 +2,20 @@ document.querySelector("html").addEventListener("click", function () {
     //alert("Ouch! Stop poking me!");
 });
 
+board = [
+    [10, 9, 8, 7, 6, 8, 9, 10],
+    [11, 11, 11, 11, 11, 11, 11, 11],
+    [-1, -1, -1, -1, -1, -1, -1, -1],
+    [-1, -1, -1, -1, -1, -1, -1, -1],
+    [-1, -1, -1, -1, -1, -1, -1, -1],
+    [-1, -1, -1, -1, -1, -1, -1, -1],
+    [5, 5, 5, 5, 5, 5, 5, 5],
+    [4, 3, 2, 0, 1, 2, 3, 4],
+];
+
 window.onload = function () {
-    const boardWidth = 8;
-    const boardHeight = 8;
+    const boardWidth = 12;
+    const boardHeight = 12;
 
     boardHtml = generateBoardHTML(boardWidth, boardHeight);
 
@@ -32,10 +43,20 @@ function generateBoardHTML(width, height) {
             var darkSquare = (x + y) % 2 > 0;
             var color = darkSquare ? "#86a666" : "#ffffdd";
             boardHtml +=
-                '<div class="square" style="background-color:' +
+                '<div class="square" style="background-position:' +
+                getPieceBackgroundPos(board[y][x]) +
+                "; background-color:" +
                 color +
-                '">' + "" + '</div>';
+                (board[y][x] == -1 ? "; background-image: none;" : "") +
+                '"></div>';
         }
     }
     return boardHtml;
+}
+
+function getPieceBackgroundPos(pieceId) {
+    x = pieceId % 6;
+    y = Math.floor(pieceId / 6);
+
+    return x * 20 + "%" + y * 100 + "%";
 }
