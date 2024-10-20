3caf579da83a05f2acf06bde6dde91a084b0445d
Phoebe J <tastygold99513@gmail.com>
2024-10-20 21:20:15 +1100

    dynamic scaling of board tiles to fit square and screen

--- script.js
+++ script.js
@@ -1,20 +1,31 @@
 document.querySelector("html").addEventListener("click", function () {
-    alert("Ouch! Stop poking me!");
+    //alert("Ouch! Stop poking me!");
 });
 
 window.onload = function () {
     const boardWidth = 8;
     const boardHeight = 8;
 
-    boardHtml = generateBoard(boardWidth, boardHeight);
-    document.querySelector(".chess").innerHTML =
+    boardHtml = generateBoardHTML(boardWidth, boardHeight);
+
+    var chessDiv = document.querySelector(".chess");
+
+    chessDiv.innerHTML =
         '<div class="grid-container" style="grid-template-columns:repeat(' +
         boardWidth +
         ', auto)"></div>';
     document.querySelector(".grid-container").innerHTML = boardHtml;
+    document.styleSheets[0].insertRule(
+        ".square{ width: calc(min(80vh, 80vw) / " +
+            boardWidth +
+            ");  height: calc(min(80vh, 80vw) / " +
+            boardHeight +
+            "); }",
+        0
+    );
 };
 
-function generateBoard(width, height) {
+function generateBoardHTML(width, height) {
     var boardHtml = "";
     for (let y = 0; y < height; y++) {
         for (let x = 0; x < width; x++) {
