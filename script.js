6f738955aa0d588656f4524d549de03d00d83ab3
Phoebe J <tastygold99513@gmail.com>
2024-10-21 17:11:26 +1100

    mousedown and mouseup piece moving

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ document.querySelector("html").addEventListener("click", function () {
 });
 
 board = [
-    [10, 9, 8, 7, 6, 8, 9, 10],
+    [10, 9, 8, 6, 7, 8, 9, 10],
     [11, 11, 11, 11, 11, 11, 11, 11],
     [-1, -1, -1, -1, -1, -1, -1, -1],
     [-1, -1, -1, -1, -1, -1, -1, -1],
@@ -13,13 +13,19 @@ board = [
     [4, 3, 2, 0, 1, 2, 3, 4],
 ];
 
-window.onload = function () {
-    const boardWidth = 8;
-    const boardHeight = 8;
+boardWidth = 8;
+boardHeight = 8;
 
+heldPieceId = -1;
+
+window.onload = function () {
+    resetBoard();
+};
+
+function resetBoard() {
     boardHtml = generateBoardHTML(boardWidth, boardHeight);
 
-    var chessDiv = document.querySelector(".chess");
+    let chessDiv = document.querySelector(".chess");
 
     chessDiv.innerHTML =
         '<div class="grid-container" style="grid-template-columns:repeat(' +
@@ -34,14 +40,22 @@ window.onload = function () {
             "); }",
         0
     );
-};
+
+    let squares = document.querySelectorAll(".square");
+    for (let i = 0; i < squares.length; i++) {
+        squares[i].addEventListener("mousedown", onSquarePressed);
+        squares[i].addEventListener("mouseup", onSquareRelease);
+        squares[i].squareX = i % boardWidth;
+        squares[i].squareY = Math.floor(i / boardWidth);
+    }
+}
 
 function generateBoardHTML(width, height) {
-    var boardHtml = "";
+    let boardHtml = "";
     for (let y = 0; y < height; y++) {
         for (let x = 0; x < width; x++) {
-            var darkSquare = (x + y) % 2 > 0;
-            var color = darkSquare ? "#86a666" : "#ffffdd";
+            let darkSquare = (x + y) % 2 > 0;
+            let color = darkSquare ? "#86a666" : "#ffffdd";
             boardHtml +=
                 '<div class="square" style="background-position:' +
                 getPieceBackgroundPos(board[y][x]) +
@@ -59,4 +73,24 @@ function getPieceBackgroundPos(pieceId) {
     y = Math.floor(pieceId / 6);
 
     return x * 20 + "%" + y * 100 + "%";
+}
+
+function onSquarePressed(evt) {
+    let x = evt.currentTarget.squareX;
+    let y = evt.currentTarget.squareY;
+    if (board[y][x] != -1) {
+        heldPieceId = board[y][x];
+        board[y][x] = -1;
+    }
+    resetBoard();
+}
+
+function onSquareRelease(evt) {
+    let x = evt.currentTarget.squareX;
+    let y = evt.currentTarget.squareY;
+    if (heldPieceId != -1) {
+        board[y][x] = heldPieceId;
+        heldPieceId = -1;
+    }
+    resetBoard();
 }
